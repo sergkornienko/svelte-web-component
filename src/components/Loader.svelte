44a96531b67caf8977d1b3adfc523b941f52295c
serhiiko <serhiiko@wix.com>
2020-08-01 19:32:33 +0300

    ➕ add loader

---
+++ src/components/Loader.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let centred;
+  export let width;
+</script>
+
+<img
+  src="https://static.wixstatic.com/media/6b6193_a9c819b601db4a78b964470f8cc68826~mv2.gif"
+  alt="Loader"
+  class="loader"
+  class:centred
+  {width}
+>
+
+<style>
+  .centred {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
