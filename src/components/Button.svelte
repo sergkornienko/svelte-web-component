b780bdbb7fca0a46ff2eb359e6adc973735b20cc
serhiiko <serhiiko@wix.com>
2020-08-02 12:49:30 +0300

    ➕ add button

---
+++ src/components/Button.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let label;
+  export let primary;
+  export let disabled;
+</script>
+
+<button
+  class="btn"
+  class:primary
+  {disabled}
+>
+  {label}
+</button>
+
+<style>
+  .btn {
+    font-style: normal;
+    font-size: 16px;
+    text-decoration: none;
+    cursor: pointer;
+    outline: none;
+    white-space: nowrap;
+    user-select: none;
+    transition: all 0.1s ease-in-out;
+    -webkit-touch-callout: none;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+  .primary {
+    border-radius: 100px;
+    padding: 8px 28px;
+    border-width: 1px;
+    border-style: solid;
+    display: inline-block;
+    background-color: #3d9be9;
+    border-color: transparent;
+    margin: 0;
+    color: #fff;
+  }
+  .primary:hover {
+    background-color: #4eb7f5;
+  }
+  .primary:disabled {
+    background-color: #ccc;
+    pointer-events: none;
+    cursor: default;
+  }
+</style>
--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -1,8 +1,10 @@
 <script>
  import Input from './Input.svelte';
+ import Button from './Button.svelte';
  import debounce from 'lodash/debounce';
 
   const handleSearchInput = debounce((e) => console.log(e.target.value), 500);
+  const handleRefreshClick = (e) => console.log(e);
 </script>
   
 <div class="toolbar">
@@ -10,6 +12,12 @@
     search
     placeholder="Search Conversations"
     on:input={handleSearchInput}
+  />
+  <Button
+    primary
+    label="Refresh"
+    on:click={handleRefreshClick}
+    disabled={false}
   />
 </div>
 
