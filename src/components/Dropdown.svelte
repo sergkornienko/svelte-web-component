9d8053d880db62154b70dc71335f490e4821051c
serhiiko <serhiiko@wix.com>
2020-08-02 14:41:12 +0300

    ➕ add dropdown

---
+++ src/components/Dropdown.svelte
@@ -0,0 +1,49 @@
+<script>
+  import arrowDown from '../assets/arrow-down.svg';
+  
+  export let options;
+  export let name;
+  export let value;
+  // BUG: arrow click dosent open options
+</script>
+
+<div class="dropdown">
+  <select 
+    class="input-element"
+    {name}
+    bind:value
+  >
+    {#each options as {label, value}}
+      <option {value}>{label}</option>
+    {/each}
+  </select>
+  <div class="arrow">
+    {@html arrowDown}
+  </div>
+</div>
+
+<style>
+  .dropdown {
+    display: inline-block;
+    position: relative;
+    width: fit-content;
+  }
+  select {
+    border-radius: 10px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    padding-right: 30px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .arrow {
+    position: absolute;
+    top: 9px;
+    right: 9px;
+  }
+  :global(.arrow-down-icon) {
+    height: 10px;
+    fill: var(--link-color);
+  }
+</style>
--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -1,7 +1,14 @@
 <script>
- import Input from './Input.svelte';
- import Button from './Button.svelte';
- import debounce from 'lodash/debounce';
+  import Input from './Input.svelte';
+  import Button from './Button.svelte';
+  import Dropdown from './Dropdown.svelte';
+  import debounce from 'lodash/debounce';
+
+  const options = [{
+    label: 'Meh',
+    value: 0,
+  }];
+  let filter;
 
   const handleSearchInput = debounce((e) => console.log(e.target.value), 500);
   const handleRefreshClick = (e) => console.log(e);
@@ -19,11 +26,18 @@
     on:click={handleRefreshClick}
     disabled={false}
   />
+  <Dropdown
+    label="Blet"
+    {options}
+    bind:value={filter}
+  />
+  {filter}
 </div>
 
 <style>
   .toolbar {
     background-color: #fff;
     padding: 14px;
+    box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.3);
   }
 </style>
