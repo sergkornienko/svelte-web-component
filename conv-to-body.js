f8b2f030c58d99e6360abd0763679b3382980c87
serhiiko <serhiiko@wix.com>
2020-08-03 12:06:03 +0300

    Updates

--- conv-to-body.js
+++
@@ -1,9 +0,0 @@
-
-const convInt = setInterval(() => {
-	const item = document.querySelector('chatbot-conv');
-	if (item) {
-		document.body.appendChild(item);
-		console.log('move to the body');
-		clearInterval(convInt);
-	}
-}, 100);
