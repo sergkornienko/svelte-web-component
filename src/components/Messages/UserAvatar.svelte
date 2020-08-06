5953564a8199ef60056481fc08ff4c104ecd970d
serhiiko <serhiiko@wix.com>
2020-08-06 14:49:37 +0300

    🐛 bug with user avatar

--- src/components/Messages/UserAvatar.svelte
+++ src/components/Messages/UserAvatar.svelte
@@ -10,4 +10,9 @@
   .user-avatar {
     right: -40px;
   }
+  img {
+    background: url(https://static.wixstatic.com/media/e3b156_3d201b5439cc4306b29eec0c64e816b1~mv2.png);
+    background-repeat: no-repeat;
+    background-size: 24px;
+  }
 </style>
